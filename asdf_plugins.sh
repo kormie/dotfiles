b56a0871eccf10c627d6b2fa9c132c77f1c09680
David Kormushoff <kormie@gmail.com>
2017-03-01 17:03:08 -0600

    Add default plugins

---
+++ asdf_plugins.sh
@@ -0,0 +1,45 @@
+# Move to $HOME so globals go to the right place
+pushd ~ > /dev/null 2>$1
+
+# Add all plugins and update them
+asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
+asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
+asdf plugin-add elm https://github.com/vic/asdf-elm.git
+asdf plugin-add node https://github.com/asdf-vm/asdf-nodejs.git
+asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
+asdf plugin-add go https://github.com/kennyp/asdf-golang.git
+asdf plugin-add haskell https://github.com/vic/asdf-haskell.git
+
+asdf plugin-update --all
+
+# Setup global versions
+GLOBAL_ERLANG=19.2
+GLOBAL_ELIXIR=1.4.0
+GLOBAL_ELM=0.18.0
+GLOBAL_GO=1.8
+GLOBAL_HASKELL=8.0.2
+GLOBAL_NODE=6.10.0
+GLOBAL_RUBY=2.4.0
+
+read -d '' GLOBALS <<EOF
+erlang $GLOBAL_ERLANG
+elixir $GLOBAL_ELIXIR
+elm $GLOBAL_ELM
+go $GLOBAL_GO
+haskell $GLOBAL_HASKELL
+node $GLOBAL_NODE
+ruby $GLOBAL_RUBY
+EOF
+
+# Set Global Versions
+echo $GLOBALS > .tool-versions
+
+# Install global versions
+asdf install
+
+# Add other versions
+# asdf install elm 0.17.0
+
+# Return the user to prior location
+popd > /dev/null 2>$1
+
