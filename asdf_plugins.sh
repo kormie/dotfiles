91933c9f607365567e2aab37c024b1b642a4a075
David Kormushoff <kormie@gmail.com>
2017-03-01 19:14:45 -0600

    [00] Get stupid node plugin working

--- asdf_plugins.sh
+++ asdf_plugins.sh
@@ -1,16 +1,43 @@
+function handle_node_key_check() {
+  echo "Checking PGP Keys for node..."
+  alias check_rings=". ~/.asdf/plugins/nodejs/bin/import-release-team-keyring"
+  set +m
+  check_rings > /dev/null 2>&1 &
+  bg_pid=$!
+
+  while ps | grep -v grep | grep -q $bg_pid
+  do
+    echo Keys are still processing
+    sleep 3
+  done
+
+  wait $bg_pid
+  exit_code=$?
+  set -m
+  if [ $exit_code -ne 0 ]
+  then
+    echo "Issues with PGP keys"
+    return $exit_code 2> /dev/null || exit $exit_code
+  fi
+
+  echo "PGP Keys look good!"
+}
 # Move to $HOME so globals go to the right place
-pushd ~ > /dev/null 2>$1
+pushd $HOME > /dev/null 2>&1
 
 # Add all plugins and update them
 asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
 asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
 asdf plugin-add elm https://github.com/vic/asdf-elm.git
-asdf plugin-add node https://github.com/asdf-vm/asdf-nodejs.git
+asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
 asdf plugin-add go https://github.com/kennyp/asdf-golang.git
 asdf plugin-add haskell https://github.com/vic/asdf-haskell.git
 
 asdf plugin-update --all
+
+# Do stupid PGP setup for node
+handle_node_key_check
 
 # Setup global versions
 GLOBAL_ERLANG=19.2
@@ -27,7 +54,7 @@ elixir $GLOBAL_ELIXIR
 elm $GLOBAL_ELM
 go $GLOBAL_GO
 haskell $GLOBAL_HASKELL
-node $GLOBAL_NODE
+nodejs $GLOBAL_NODE
 ruby $GLOBAL_RUBY
 EOF
 
@@ -38,8 +65,9 @@ echo $GLOBALS > .tool-versions
 asdf install
 
 # Add other versions
-# asdf install elm 0.17.0
+asdf install elm 0.17.0
 
 # Return the user to prior location
-popd > /dev/null 2>$1
+popd > /dev/null 2>&1
+
 
